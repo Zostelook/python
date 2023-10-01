53a10bf52dc18a237090ee771d5e622f6b01ddba
Abhishek Kumar <litecode420@gmail.com>
2023-10-01 17:15:19 +0000

    Frist program

--- Test.py
+++ Test.py
@@ -1,1 +1,14 @@
 print("This is my first python file , here I am trying to set my codespace on github")
+
+
+#This is the commend in python
+
+#print("hello world")   this code will not run
+
+#how we can create variable in python
+
+myAccountNumber = 397848340
+print(myAccountNumber)
+
+myName = "Abhishek"
+print(myName)
